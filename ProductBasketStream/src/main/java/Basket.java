f6f4fea88effca1bad40281999e4ee81c47eec68
GarRustem <rustemgarifullin.mail@gmail.com>
2023-01-15 20:56:00 +0500

    First v.

---
+++ ProductBasketStream/src/main/java/Basket.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Basket implements Serializable {
+
+    protected String[] products = {"Milk", "Coffee", "Sugar"};
+    protected int[] prices = {20, 300, 150};
+    protected int[] goodsQuantity = new int[3];
+    protected int count;
+    protected int totalPrice = 0;
+    protected int product;
+    protected int quantity;
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.goodsQuantity = new int[products.length];
+        this.count = 0;
+    }
+
+    private Basket() {
+    } // Модификатор доступа private для того, чтобы нельзя было создать пустую корзину (не указывая ассортимент) - только для внутренней логики программы в рамках открытия файла существующей корзины, когда еще не сделаны заказы, а нужно восстановить корзину из файла.
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void addToCard(int count, int[] goodsQuantity) {
+
+        System.out.println("Goods, available for sale:");
+
+        for (int i = 0; i < products.length; i++) {
+            String k = products[i];
+            int j = prices[i];
+            count++;
+            System.out.println(count + ". " + k + " for " + j + " RUR/100g");
+        }
+
+        while (true) {
+            System.out.println("Please, enter the name of the product and its quantity, separated by a comma, or type end.");
+            String input = scanner.nextLine();
+            String delimiter = ",";
+
+            if ("end".equals(input)) {
+                break;
+            }
+
+            if (input.contains(delimiter)) {
+                String[] parts = input.split(",");
+
+                if (parts.length != 2) {
+                    System.out.println("The order must contains one position from product list and one number of quantity for this product. Please, try again.");
+                    continue;
+                }
+
+                try {
+                    product = Integer.parseInt(parts[0]);
+                    quantity = Integer.parseInt(parts[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("You must type only digits, separated by comma, not symbols");
+                    break;
+                }
+
+                if (product > 3 || quantity < 1 || quantity > 9) {
+                    System.out.println("Please, enter a right option from product list: numbers 1-3 for product type and number 1-9 for its quantity.");
+                    continue;
+                }
+
+                int productPrice = prices[product - 1];
+                goodsQuantity[product - 1] += quantity;
+                totalPrice += productPrice * quantity;
+            } else {
+                System.out.println("You must input product number and its quantity, separated by comma in format NUMBER,NUMBER");
+            }
+        }
+    }
+
+    public void printCart() {
+        System.out.println("Your order list is:");
+        for (int i = 0; i <= goodsQuantity.length - 1; i++) {
+            if (goodsQuantity[i] != 0) {
+                System.out.println(products[i] + " " + goodsQuantity[i] + " pieces for " + prices[i] + " RUR/100g is "
+                        + goodsQuantity[i] * prices[i] + " RUR total");
+            }
+        }
+        System.out.println("Total price is: " + totalPrice);
+    }
+
+    public void saveTextFile(File file) throws IOException {
+        try (PrintWriter output = new PrintWriter(file);) {
+            for (int i = 0; i <= goodsQuantity.length - 1; i++) {
+                if (goodsQuantity[i] != 0) {
+                    output.println(products[i]);
+                    output.println(prices[i]);
+                    output.println(goodsQuantity[i]);
+                }
+            }
+        }
+    }
+    public static Basket loadFromTextFile(File file) throws IOException {
+        try (BufferedReader basketFromFile = new BufferedReader(new FileReader(file))) {
+            Basket basket = new Basket();
+            for (int i = 0; i < basket.products.length; i++) {
+                while(basketFromFile.readLine() != null) {
+                    basket.products[i] = basketFromFile.readLine();
+                }
+            }
+            for (int i = 0; i < basket.products.length; i++) {
+                while(basketFromFile.readLine() != null) {
+                    basket.prices[i] = Integer.parseInt(basketFromFile.readLine());
+                }
+            }
+            for (int i = 0; i < basket.products.length; i++) {
+                while(basketFromFile.readLine() != null) {
+                    basket.goodsQuantity[i] = Integer.parseInt(basketFromFile.readLine());
+                }
+            }
+            return basket;
+        }
+    }
+}
---
+++ ProductBasketStream/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String[] products = {"Milk", "Coffee", "Sugar"};
+        int[] prices = {20, 300, 150};
+        Basket basket = new Basket(products, prices);
+        File file = new File(".//productBasket.txt");
+
+//        try {
+            if (file.exists()) {
+                Basket.loadFromTextFile(file);
+                basket.printCart();
+            } else {
+                file.createNewFile();
+            }
+//        } catch (Exception e) {
+//            System.out.println("Program is broken");
+//        }
+
+        basket.addToCard(basket.count, basket.goodsQuantity);
+
+        basket.printCart();
+
+        basket.saveTextFile(file);
+    }
+}
